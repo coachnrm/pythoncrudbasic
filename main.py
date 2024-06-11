05c0f2c76da6a74b0c5ce5af45fcae263c07cfed
home <nuttapong_coach@hotmail.com>
2024-06-11 22:39:14 +0700

    add html page

--- main.py
+++ main.py
@@ -1,36 +1,6 @@
-from pymongo import MongoClient
+from website import create_app
 
-client = MongoClient('localhost', 27017)
+app = create_app()
 
-# Get the database
-db = client.mydb
-
-# Create collection
-todo = db.todo
-
-print(db)
-
-# Insert a document
-# todo_1 = {
-#    'title': 'Buy milk',
-#    'description': 'Milk is needed for the baby',
-#    'done': False
-# }
-
-# todo.insert_one(todo_1)
-
-# Get a document
-# todo_1 = todo.find()
-
-# print(list(todo_1))
-
-# Update a document
-# todo.update_one({"title": "Buy milk"}, {"$set": {"done": True}})
-# todo_1 = todo.find()
-
-# print(list(todo_1))
-
-# Delete a document
-todo.delete_many({"title": "Buy milk"})
-todo_1 = todo.find()
-print(list(todo_1))
+if __name__ == '__main__':
+    app.run(debug=True)
